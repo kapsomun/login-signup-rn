6c549e6a2e96d8d12eea047fa6f0e8daad7da1be
igor-front <parsival312@mail.ru>
2024-06-10 17:08:03 +0300

    feat: start navigation block

--- app/screens/splash/index.tsx
+++ app/screens/splash/index.tsx
@@ -42,3 +42,88 @@ export default function SplashScreenStack() {
 
     return <View ><Text>hello</Text></View>;
 }
+// https://github.com/mrousavy/react-native-mmkv
+
+
+// [14:58] Bogdan React Native developer
+// const RootNavigator: FC = () => {
+//   return (
+//       <RootStack.Navigator initialRouteName={routes.SplashScreen}>
+//         <RootStack.Screen
+//           name={routes.SplashScreen}
+//           component={SplashScreen}
+//           options={{headerShown: false, orientation: 'portrait'}}
+//         />
+//         <RootStack.Screen
+//           name={routes.LoginStack}
+//           component={LoginStackScreens}
+//           options={{headerShown: false, orientation: 'portrait'}}
+//         />
+//         <RootStack.Screen
+//           name={routes.HomeStack}
+//           component={HomeStackScreens}
+//           options={{headerShown: false, orientation: 'portrait'}}
+//         />
+//       </RootStack.Navigator>
+//   );
+// };
+// [14:58] Bogdan React Native developer
+// const RootStack = createNativeStackNavigator();
+// const HomeStack = createNativeStackNavigator();
+// const LoginStack = createNativeStackNavigator();
+// [15:00] Bogdan React Native developer
+// const HomeStackScreens = () => {
+//   return (
+//     <HomeStack.Navigator initialRouteName={routes.Home}>
+//       <HomeStack.Screen
+//         name={routes.Home}
+//         component={() => <View></View>}//твоя компонента хоум
+//         options={{headerShown: false, orientation: 'portrait'}}
+//       />
+//     </HomeStack.Navigator>
+//   );
+// };
+// [15:02] Bogdan React Native developer
+// const LoginStackScreens = () => {
+//   return (
+//     <LoginStack.Navigator initialRouteName={routes.Home}>
+//       <LoginStack.Screen
+//         name={routes.Home}
+//         component={() => <View></View>}//твоя компонента логін
+//         options={{headerShown: false, orientation: 'portrait'}}
+//       />
+//     </LoginStack.Navigator>
+//   );
+// };
+// [15:54] Bogdan React Native developer
+// export default {
+//   SplashScreen: 'SplashScreenStack' as const,
+//   Onboarding: 'OnboardingScreen' as const,
+//   LoginStack: 'LoginStack' as const,
+//   SignUpPhoneNumber: 'SignUpPhoneNumber' as const,
+//   HomeStack: 'HomeStack' as const,
+//   Home: 'HomeScreen' as const,
+// };
+ 
+// export type RootStackParamList = {
+//   SplashScreen: undefined;
+//   Onboarding: undefined;
+//   LoginStack: undefined;
+//   SignUpPhoneNumber: undefined;
+//   HomeStack: undefined;
+//   Home: undefined;
+// };
+ 
+// [16:07] Bogdan React Native developer
+//   useFocusEffect(
+//     useCallback(() => {
+//       const onBackPress = () => {
+//         return true
+//       }
+ 
+//       BackHandler.addEventListener('hardwareBackPress', onBackPress)
+ 
+//       return () =>
+//         BackHandler.removeEventListener('hardwareBackPress', onBackPress)
+//     }, []),
+//   )
